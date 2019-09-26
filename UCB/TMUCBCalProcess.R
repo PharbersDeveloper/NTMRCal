cf41a5c095ab7a78517f9669f427dbcbb3b42be1
AlfredYang1986 <yangyuanpig@gmail.com>
2019-09-26 11:43:38 +0800

    试试

--- UCB/TMUCBCalProcess.R
+++ UCB/TMUCBCalProcess.R
@@ -273,7 +273,7 @@ TMUCBCalProcess <- function(
     cal_data <- mutate(cal_data,
                        next_budget = cal_next_budget(cal_data))
 
-    cal_data <- repartition(cal_data, numPartitions = 1L)
+    # cal_data <- repartition(cal_data, numPartitions = 1L)
     persist(cal_data, "MEMORY_ONLY")
     up_result <- cal_data
     
@@ -330,7 +330,7 @@ TMUCBCalProcess <- function(
     )
    
     #cal_result_summary <- repo 
-    cal_product_area <- repartition(cal_data, numPartitions = 1L)
+    cal_result_summary <- repartition(cal_result_summary, numPartitions = 1L)
     cal_result_summary <- TMAggSchema(cal_result_summary, 
                                  c(list(name=c("p_sales", "pppp_sales", "sales", "quota", "account", "budget")), "sum"),
                                  group_by_rs, 
